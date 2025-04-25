06e041ad2da12ca0740460dfe505ad97f676faec
songezop01 <31156369+songezop01@users.noreply.github.com>
2025-04-25 17:41:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io'); // 正確引入 Server 類
+const { Server } = require('socket.io');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: '*', // 允許所有來源，僅用於測試
+        origin: '*',
         methods: ['GET', 'POST']
     },
-    transports: ['polling', 'websocket'] // 確保支持 polling 和 websocket
+    transports: ['polling', 'websocket']
 });
 
 const users = new Map();
